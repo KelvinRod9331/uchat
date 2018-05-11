44e096a9b649250cf6ce5dd527c41906e494b701
KelvinRod9331 <kelvinrodriguez@ac.c4q.nyc>
2018-05-11 19:47:36 -0400

    Added A New Style

--- frontend/src/Chat/ChatRoom/SingleMessage.js
+++ frontend/src/Chat/ChatRoom/SingleMessage.js
@@ -4,42 +4,38 @@ import "./SingleMessage.css";
 
 const SingleMessage = ({ threadMessages, currentUser, contact, sendingMessages, receivingMessages }) => {
   console.log('Single Message', threadMessages, currentUser)
-  let currentUserMessages = {}
-  let otherUserMessages = {}
-  
-  threadMessages.forEach(messenger => {
-    if (currentUser.id === messenger.receiver_id) {
-      currentUserMessages.message = messenger.receiver_messages
-      currentUserMessages.username = currentUser.username
-    } else if (currentUser.id === messenger.sender_id) {
-      currentUserMessages.message = messenger.sender_messages
-      otherUserMessages.username = contact.username
+ 
+
+  return threadMessages.map(messenger => {
+    if (currentUser.id === messenger.sender_id) {
+      return (
+        <div className={"currentUserMessage"}>
+          <div className="message-header">
+            {/* <div className="message-date">{dateFormat(message.date_sent)}</div> */}
+            <div className="message-sender">{currentUser.username}</div>
+          </div>
+          <div className="message-body-container">
+            <div className="message-body-content">{messenger.sender_message}</div>
+          </div>
+        </div>
+      );
+      
+    } else if (currentUser.id === messenger.receiver_id) {
+      return (
+        <div className={"otherUserMessage"}>
+          <div className="message-header">
+            {/* <div className="message-date">{dateFormat(message.date_sent)}</div> */}
+            <div className="message-sender">{contact.username}</div>
+          </div>
+          <div className="message-body-container">
+            <div className="message-body-content">{messenger.receiver_message}</div>
+          </div>
+        </div>
+      );
     }
   });
-  
-      if(contact.username === currentUser.username){
-     return (<div className={"currentUserMessage"}>
-        <div className="message-header">
-          {/* <div className="message-date">{dateFormat(message.date_sent)}</div> */}
-          <div className="message-sender">{currentUserMessages.username}</div>
-        </div>
-        <div className="message-body-container">
-          <div className="message-body-content">{currentUserMessages.message}</div>
-        </div>
-      </div>)
-      }
-      else{
-     return (<div className={"otherUserMessage"}>
-        <div className="message-header">
-          {/* <div className="message-date">{dateFormat(message.date_sent)}</div> */}
-          <div className="message-sender">{otherUserMessages.username}</div>
-        </div>
-        <div className="message-body-container">
-          <div className="message-body-content">{otherUserMessages.message}</div>
-        </div>
-      </div>)
-      }
 
 };
 
 export default SingleMessage;
+
